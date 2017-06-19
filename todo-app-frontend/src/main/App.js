493f72c972196cf638e119b179e3ebfc24e694d1
daiCorrea <daiane.ocorrea@gmail.com>
2017-06-19 11:15:05 -0300

    Adding Bulma and Font-awesome

--- todo-app-frontend/src/main/App.js
+++ todo-app-frontend/src/main/App.js
@@ -5,14 +5,31 @@ import Todo from '../container/todo';
 import About from '../container/about';
 import Menu from '../component/menu';
 
+import '../style/bulma/bulma.css'
+
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <Menu />
-        <Todo />
-        <About />
+      <div className="container">
+          <div className="notification">
+            <div className="block">
+              <a className="button">Button</a>
+              <a className="button is-white">White</a>
+              <a className="button is-light">Light</a>
+              <a className="button is-dark">Dark</a>
+              <a className="button is-black">Black</a>
+              <a className="button is-link">Link</a>
+            </div>
+            <div className="block">
+              <a className="button is-primary">Primary</a>
+              <a className="button is-info">Info</a>
+              <a className="button is-success">Success</a>
+              <a className="button is-warning">Warning</a>
+              <a className="button is-danger">Danger</a>
+            </div>
+              This container is <strong>centered</strong> on desktop.
+          </div>
       </div>
     );
   }
